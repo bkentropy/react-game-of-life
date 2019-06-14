f31bf0289c20e650cea8a1896de49a70b83dddec
Brian Kustra <bkustrakb@gmail.com>
2019-06-13 22:48:47 -0500

    Fixed npm start. Still working on firebase

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
