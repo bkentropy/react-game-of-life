a6eb5b6ad84c0dc135ee0f5b9932bf586f1c6699
Brian Kustra <bkustrakb@gmail.com>
2019-08-15 16:43:10 -0500

    Side bar to implement the idea of "lifting state"

--- src/components/conways.js
+++ src/components/conways.js
@@ -79,13 +79,106 @@ function RowDiv(props) {
 
 //   return map;
 // }
+const scaleNames = {
+    c: "Celsius",
+    f: "Fahrenheit"
+}
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit -32) * 5 / 9
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature)
+    if (Number.isNaN(input)) {
+        return ""
+    }
+    const output = convert(input)
+    const rounded = Math.round(output * 1000) / 1000
+    return rounded.toString()
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value)
+    }
+
+    render() {
+        const temperature = this.props.temperature
+        const scale = this.props.scale
+        return( 
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                        onChange={this.handleChange} />
+            </fieldset>
+        )
+ 
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this)
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this)
+        this.state = {temperature: "", scale: "c"}
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: "c", temperature})
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: "f", temperature})
+    }
+
+    render() {
+        const scale = this.state.scale
+        const temperature= this.state.temperature
+        const celsius = scale === "f" ? tryConvert(temperature, toCelsius) : temperature
+        const fahrenheit = scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature
+
+        return (
+            <div>
+                <div>
+                    <TemperatureInput 
+                        scale="c" 
+                        temperature={celsius}
+                        onTemperatureChange={this.handleCelsiusChange} />
+                    <TemperatureInput 
+                        scale="f"
+                        temperature={fahrenheit}
+                        onTemperatureChange={this.handleFahrenheitChange} />
+                </div>
+
+                <BoilingVerdict celsius={parseFloat(temperature)} />
+            </div>
+        )
+    }
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
 export class GameOfLife extends React.Component {
     constructor(props) {
         super(props)
         this.state = {current: new Array(10).fill(new Array(10).fill(0))}
     }
-
-
 
     render() {
         var rows = nums.map(i => {
@@ -97,6 +190,9 @@ export class GameOfLife extends React.Component {
                 <h1>Game Of Life</h1>
                 <div id="gameDiv">
                     {rows}
+                </div>
+                <div>
+                    <Calculator />
                 </div>
             </div>
         )
