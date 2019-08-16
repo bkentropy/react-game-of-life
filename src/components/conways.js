0e6b50c3cc84038514b5ff7d17efbc8808d78371
Brian Kustra <bkustrakb@gmail.com>
2019-08-16 09:58:43 -0500

    Removing temperature tangent, passing function down through components

--- src/components/conways.js
+++ src/components/conways.js
@@ -11,9 +11,13 @@ class Square extends React.Component {
         super(props)
         this.state = {alive: false}
         this.turnGreen = this.turnGreen.bind(this);
+        // this.handleSquareClick = this.handleSquareClick.bind(this)
     }
 
+    // handleSquareClick(i, j) { }
+
     turnGreen() {
+        this.props.onSquareClick(0, 0)
         if (!this.state.alive) {
             this.setState(state => ({alive: true}))
         } else {
@@ -38,8 +42,12 @@ function RowDiv(props) {
         var col = "column" + i
         var classes = "golsquare " + col
         var k = props.id + col
+        console.log(props.onSquareClick)
         return (
-            <Square key={k} classes={classes}/>
+            <Square 
+                key={k} 
+                classes={classes} 
+                onSquareClick={props.onSquareClick} />
         )
     })
 
@@ -79,110 +87,24 @@ function RowDiv(props) {
 
 //   return map;
 // }
-const scaleNames = {
-    c: "Celsius",
-    f: "Fahrenheit"
-}
-
-function toCelsius(fahrenheit) {
-    return (fahrenheit -32) * 5 / 9
-}
-
-function toFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32
-}
-
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature)
-    if (Number.isNaN(input)) {
-        return ""
-    }
-    const output = convert(input)
-    const rounded = Math.round(output * 1000) / 1000
-    return rounded.toString()
-}
-
-class TemperatureInput extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value)
-    }
-
-    render() {
-        const temperature = this.props.temperature
-        const scale = this.props.scale
-        return( 
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature}
-                        onChange={this.handleChange} />
-            </fieldset>
-        )
- 
-    }
-}
-
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this)
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this)
-        this.state = {temperature: "", scale: "c"}
-    }
-
-    handleCelsiusChange(temperature) {
-        this.setState({scale: "c", temperature})
-    }
-
-    handleFahrenheitChange(temperature) {
-        this.setState({scale: "f", temperature})
-    }
-
-    render() {
-        const scale = this.state.scale
-        const temperature= this.state.temperature
-        const celsius = scale === "f" ? tryConvert(temperature, toCelsius) : temperature
-        const fahrenheit = scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature
-
-        return (
-            <div>
-                <div>
-                    <TemperatureInput 
-                        scale="c" 
-                        temperature={celsius}
-                        onTemperatureChange={this.handleCelsiusChange} />
-                    <TemperatureInput 
-                        scale="f"
-                        temperature={fahrenheit}
-                        onTemperatureChange={this.handleFahrenheitChange} />
-                </div>
-
-                <BoilingVerdict celsius={parseFloat(temperature)} />
-            </div>
-        )
-    }
-}
-
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
 
 export class GameOfLife extends React.Component {
     constructor(props) {
         super(props)
+        this.handleSquareClick = this.handleSquareClick.bind(this)
         this.state = {current: new Array(10).fill(new Array(10).fill(0))}
+    }
+
+    handleSquareClick(i, j) {
+        console.log(this.state.current[i][j])
+        var sqState = this.state.current[i][j] == 0 ? 1 : 0
+        console.log(sqState)
+        // this.setState
     }
 
     render() {
         var rows = nums.map(i => {
-            return <RowDiv key={i} id={i}/>
+            return <RowDiv key={i} id={i} onSquareClick={this.handleSquareClick} />
         })
         
         return (
@@ -190,9 +112,6 @@ export class GameOfLife extends React.Component {
                 <h1>Game Of Life</h1>
                 <div id="gameDiv">
                     {rows}
-                </div>
-                <div>
-                    <Calculator />
                 </div>
             </div>
         )
